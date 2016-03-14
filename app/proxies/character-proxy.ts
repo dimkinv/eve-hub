a276e5a797f3d0344a995c809f71a4d33bc3193c
Danny Vernovsky <dimkinv@gmail.com>
2016-03-14 02:06:30 +0200

    work in progress

--- app/proxies/character-proxy.ts
+++ app/proxies/character-proxy.ts
@@ -1,9 +1,16 @@
 import {Injectable} from 'angular2/core'
-import {XMLProxyBase} from './xml-proxy-base'
+import {XMLProxyBase, eEndpoints, ICharAuthentication} from './xml-proxy-base'
 
 @Injectable()
 class CharacterProxy extends XMLProxyBase{
     constructor(){
-        super.constructor();
+        super();
+    }
+
+    get(){
+        this.getXMLResult(eEndpoints.CHARACTER_INFO, {
+            keyID: '5105576',
+            vCode: 'DrVegif62MekwyI4oSU8zuGRjzzaaKCsW2WvUHJRyDhFYI8vfXNLZcPkbF75B1BZ'
+        })
     }
 }
--- app/proxies/xml-proxy-base.ts
+++ app/proxies/xml-proxy-base.ts
@@ -1,7 +1,10 @@
+import {Http} from 'angular2/http'
+import {_} from 'lodash/index'
+
 export class XMLProxyBase {
     public baseUrl:string;
 
-    constructor() {
+    constructor(private http:Http) {
         //TODO: should be moved into configurations later
         this.baseUrl = 'https://api.eveonline.com/eve/'
     }
@@ -10,17 +13,42 @@ export class XMLProxyBase {
         return this.baseUrl + (endpointSuffix[0] === '/' ? endpointSuffix : '/' + endpointSuffix);
     }
 
-    private _getXMLResult(eEndpoint: eEndpoints, queryString:string, body?:any) {
+    protected getXMLResult(eEndpoint: eEndpoints, params: ICharAuthentication) {
         let url;
 
         //TODO: this all should come from configurations
         switch (eEndpoint) {
             case eEndpoints.CHARACTER_INFO:
-                url = this.getFullEndpoint('CharacterInfo.xml.aspx')
+                url = this.getFullEndpoint('CharacterInfo.xml.aspx') + this._buildQueryString(params);
+                break;
         }
+
+        return this.http.get(url)
+            .toPromise()
+            .then(res => res.text())
+            .catch(XMLProxyBase._handleError);
+    }
+
+    private static _handleError(error: any){
+        //TODO: should be more informative
+        return Promise.reject(error);
+    }
+
+    private _buildQueryString(params: {string, string}){
+        let queryString = '?';
+        _.each(params, (val, key)=>{
+            queryString += key + '=' + val + '&';
+        });
+
+        return queryString;
     }
 }
 
 export enum eEndpoints{
     CHARACTER_INFO
+}
+
+export interface ICharAuthentication{
+    vCode: string;
+    keyID: string;
 }
